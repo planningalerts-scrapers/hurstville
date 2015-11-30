35a5b118d7aae2ea840c0096aca70a0ca14009c1
Henare Degan <henare.degan@gmail.com>
2015-11-30 11:18:19 +1100

    Revert "Temporarily get last month's applications to seed scraper"
    This reverts commit df2ae5940090388d871ec25afe3b2b5f3bedb6d5.

--- scraper.php
+++ scraper.php
@@ -64,8 +64,8 @@ function buildformdata($dom, $eventtarget) {
 
 $url_base = "http://daenquiry.hurstville.nsw.gov.au/masterviewui/Modules/applicationmaster/";
 #$da_page = $url_base . "default.aspx?page=found&1=thisweek&4a=DA%27,%27S96Mods%27,%27Mods%27,%27Reviews&6=F";
-// $da_page = $url_base . "default.aspx?page=found&1=thismonth&4a=DA%27,%27S96Mods%27,%27Mods%27,%27Reviews&6=F";        # Use this URL to get 'This Month' submitted DA, also to test pagination
-$da_page = $url_base . "default.aspx?page=found&1=lastmonth&4a=DA%27,%27S96Mods%27,%27Mods%27,%27Reviews&6=F";        # Use this URL to get 'Last Month' submitted DA, also to test pagination
+$da_page = $url_base . "default.aspx?page=found&1=thismonth&4a=DA%27,%27S96Mods%27,%27Mods%27,%27Reviews&6=F";        # Use this URL to get 'This Month' submitted DA, also to test pagination
+#$da_page = $url_base . "default.aspx?page=found&1=lastmonth&4a=DA%27,%27S96Mods%27,%27Mods%27,%27Reviews&6=F";        # Use this URL to get 'Last Month' submitted DA, also to test pagination
 $comment_base = "mailto:hccmail@hurstville.nsw.gov.au?subject=Development Application Enquiry: ";
 
 $cookies = accept_terms_get_cookies($url_base . "Default.aspx");
